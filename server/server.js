0df2d2d104009d66057f6d431e3b4d52fa4daa88
cod3zilla <paranoid.grunge@gmail.com>
2021-08-10 15:23:24 -0700

    dotenv is config only for dev env.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express=require('express')
 const cors=require('cors')
-require('dotenv').config()
+
 const connectDB=require('./config/dbconfig')
 const productsRoute=require('./router/productRoute')
 const authRoute=require('./router/authRoute')
 const orderRoute=require('./router/orderRoute')
 
 const app=express()
-const  PORT=process.env.PORT||5000
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 connectDB()
 //middlewares config
 app.use(cors())
@@ -18,20 +18,21 @@ app.use('/products',productsRoute)
 app.use('/users',authRoute)
 app.use('/order', orderRoute)
 app.get('/config/paypal', (req, res)=>{
-   res.send(process.env.PAYPAL_CLIENT_ID)
-
+  res.send(process.env.PAYPAL_CLIENT_ID)
+  
 })
 
 if(process.env.NODE_ENV === 'production'){
-   app.use(express.static(path.join(__dirname ,'/client/build')))
-   app.get('*' , (req,res) =>{
-     res.sendFile(path.resolve(__dirname , 'client','build','index.html'))
-   })
- }else{
-   app.get('/', (req, res) => {
-     res.send('API is running');
-   });
- }
+  app.use(express.static(path.join(__dirname ,'/client/build')))
+  app.get('*' , (req,res) =>{
+    res.sendFile(path.resolve(__dirname , 'client','build','index.html'))
+  })
+}else{
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
+const  PORT=process.env.PORT||5000
 app.listen(PORT,()=>console.log(`server is up on:${PORT}`))
 
